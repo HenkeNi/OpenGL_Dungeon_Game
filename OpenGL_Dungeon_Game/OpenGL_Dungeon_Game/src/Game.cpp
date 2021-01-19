b4c3d4fa3f03f7d61452143702dae53a48d86b5d
Henrik Jangefelt <henke9105@hotmail.com>
2021-01-19 01:36:10 +0100

    initializing game and handling input

--- OpenGL_Dungeon_Game/OpenGL_Dungeon_Game/src/Game.cpp
+++ OpenGL_Dungeon_Game/OpenGL_Dungeon_Game/src/Game.cpp
@@ -1,8 +1,13 @@
 #include "Game.h"
+#include "GameObject.h"
 #include "ResourceManager.h"
 #include "SpriteRenderer.h"
 
+#include "Player.h"
+#include <iostream>
+
 SpriteRenderer* Renderer;
+Player* Player2;
 
 Game::Game(unsigned int width, unsigned int height)
 	: m_state{ GameState::GAME_ACTIVE }, m_keys(), m_width { width }, m_height{ height }
@@ -11,6 +16,8 @@ Game::Game(unsigned int width, unsigned int height)
 
 Game::~Game()
 {
+	delete Renderer;
+	delete Player2;
 }
 
 void Game::init()
@@ -24,22 +31,94 @@ void Game::init()
 	// set render-specific controls	
 	Renderer = new SpriteRenderer(ResourceManager::getShader("sprite"));
 	// load textures
+	ResourceManager::loadTexture("src/textures/block_solid.png", false, "block_solid");
 	ResourceManager::loadTexture("src/textures/awesomeface.png", true, "face");
+	ResourceManager::loadTexture("src/textures/block.png", false, "block");
+	ResourceManager::loadTexture("src/textures/background.jpg", false, "background");
+	ResourceManager::loadTexture("src/textures/paddle.png", true, "paddle");
+
+	// load levels
+	GameLevel one; one.load("src/levels/one.lvl", m_width, m_height);
+
+	m_levels.push_back(one);
+	m_level = 0;
+
+
+
+	glm::vec2 playerPos = glm::vec2(
+		m_width / 2 - playerSize.x / 2.0f,
+		m_height / 2 - playerSize.y / 2.0f
+	);
+	Player2 = new Player(playerPos, playerSize, ResourceManager::getTexture("paddle"));
 
 }
 
 void Game::update(float dt)
 {
+	doCollisions();
 }
 
 void Game::processInput(float dt)
 {
+	if (m_state == GameState::GAME_ACTIVE)
+	{
+		auto currentPosition = Player2->getPosition();
+		float velocity = playerVelocity * dt;
+		
+		// move player
+		if (m_keys[GLFW_KEY_W])
+		{
+			if (currentPosition.y > 0.0f)
+				Player2->move({ currentPosition.x, currentPosition.y -= velocity });
+		}
+		if (m_keys[GLFW_KEY_S])
+		{
+			if (currentPosition.y <= m_height - Player2->getSize().y)
+				Player2->move({ currentPosition.x, currentPosition.y += velocity });
+		}
+		if (m_keys[GLFW_KEY_A])
+		{
+			if (currentPosition.x > 0.0f)
+				Player2->move({ currentPosition.x -= velocity, currentPosition.y });
+		}
+		if (m_keys[GLFW_KEY_D])
+		{
+			if (currentPosition.x <= m_width - Player2->getSize().x)
+				Player2->move({ currentPosition.x += velocity, currentPosition.y });
+		}
+	}
 }
 
 void Game::render()
 {
-	Renderer->drawSprite(ResourceManager::getTexture("face"),
-		glm::vec2(200.0f, 200.0f), glm::vec2(300.0f, 400.0f), 45.0f, glm::vec3(0.0f, 1.0f, 0.0f));
+	if (m_state == GameState::GAME_ACTIVE)
+	{
+		Renderer->drawSprite(ResourceManager::getTexture("background"),
+			glm::vec2(0.0f, 0.0f), glm::vec2(m_width, m_height), 0.0f);
+
+		m_levels[m_level].draw(*Renderer);
+
+		Player2->draw(*Renderer);
+	}
+
+}
+
+void Game::doCollisions()
+{
+	for (auto& tile : m_levels[m_level].getTiles())
+	{
+		if (tile.getIsSolid() && Player2->isColliding(tile))
+		{
+			Player2->preventCollision(tile);
+			 
+			//Player2->move(Player2->getPosition() += depth);
+			
+			
+			//std::cout << "HITTING WALL!";
+			// CHECK HOW MUCH PENETRATION IN A-AXIS AND THE Y-AXIS -> set movement left and up (for example to false)
+			// - also return the penetration and update position with currentPOsition -= penetration on both axis
+		}
+	}
 }
 
 void Game::setKey(int key, bool active)
--- OpenGL_Dungeon_Game/OpenGL_Dungeon_Game/src/Game.h
+++ OpenGL_Dungeon_Game/OpenGL_Dungeon_Game/src/Game.h
@@ -1,5 +1,14 @@
 #pragma once
 #include "GameState.h"
+#include "GameLevel.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+#include <vector>
+
+const glm::vec2 playerSize(40.0f, 40.0f);
+const float playerVelocity(500.0f);
+
 
 class Game
 {
@@ -13,11 +22,15 @@ public:
 	void update(float dt);
 	void render();
 
+	void doCollisions();
 	void setKey(int key, bool active);
 
 private:
 	GameState    m_state;
 	bool	     m_keys[1024];
 	unsigned int m_width, m_height;
+
+	std::vector<GameLevel> m_levels;
+	unsigned int		   m_level{ 0 };
 };
 
